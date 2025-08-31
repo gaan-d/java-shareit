0fb2c6ba6e3a5eb3b2dd6db17368d1eb10dd2bc3
Danil <ynx-gaan-d@yandex.ru>
2025-08-31 12:55:59 +0500

    fix checkstyle

--- gateway/src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
@@ -18,9 +18,11 @@ public class UpdateItemDto {
     public boolean hasName() {
         return !(name == null || name.isBlank());
     }
+
     public boolean hasDescription() {
         return !(description == null || description.isBlank());
     }
+
     public boolean hasAvailable() {
         return !(available == null);
     }
